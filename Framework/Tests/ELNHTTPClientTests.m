df5b607955e6c0f98b8041bc9ffc015e21d1a8a8
chebur <chebur.mail@gmail.com>
2016-04-12 19:17:33 +0300

    Add Tests target

---
+++ Framework/Tests/ELNHTTPClientTests.m
@@ -0,0 +1,23 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Dmitry Nesterenko on 12.04.16.
+//  Copyright © 2016 e-legion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "ELNHTTPClient+RACExtensions.h"
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
